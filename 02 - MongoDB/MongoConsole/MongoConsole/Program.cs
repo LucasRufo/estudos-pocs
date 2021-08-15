dee3ba7e324f4e27b4d2a3add0c724a1b083dc98
LucasRufo <rifelu25@outlook.com>
2021-08-15 00:23:36 -0300

    Adicionando os projetos referentes ao curso M220

---
+++ 02 - MongoDB/MongoConsole/MongoConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace MongoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connString = "mongodb+srv://m220student:m220password@cluster0.lq11o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+            var client = new MongoClient(connString);
+
+            var db = client.GetDatabase("sample_mflix");
+            var collection = db.GetCollection<BsonDocument>("movies");
+
+            var result = collection.Find(new BsonDocument())
+               .SortByDescending(m => m["runtime"])
+               .Limit(10)
+               .ToList();
+
+            foreach (var r in result)
+            {
+                Console.WriteLine(r.GetValue("title"));
+            }
+        }
+    }
+}
